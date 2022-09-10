7551fe3fdf90c0db69b54df2882cafffff4b470b
yjein <yjein4220@naver.com>
2022-09-10 14:37:52 +0900

    feat: Form add
    - SignUpPage에 있는 form태그를 Form 컴포넌트로 변경

--- frontend/src/components/pages/SignUpPage.tsx
+++ frontend/src/components/pages/SignUpPage.tsx
@@ -9,45 +9,69 @@ import {
   IoPersonOutline,
   IoSchoolOutline,
 } from 'react-icons/io5';
+import Form from '../ui/molecules/Form';
 
 const SignUpPage: React.FC<any> = () => {
   return (
-    <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    }}>
-      <div style={{
+    <div
+      style={{
         display: 'flex',
-        flexDirection: 'column',
-        width: 'fit-content',
-        gap: '12rem',
-        margin: '0 auto',
-      }}>
-        <Logo />
-        <form style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <div
+        style={{
           display: 'flex',
           flexDirection: 'column',
           width: 'fit-content',
-          gap: '5rem',
-        }}>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-end',
-            gap: '2rem',
-          }}>
-            <IconInput type={'text'} name={'id'} placeholder={'아이디'}><IoPersonOutline /></IconInput>
-            <IconInput type={'password'} name={'password'} placeholder={'비밀번호'}><IoLockClosedOutline /></IconInput>
-            <IconInput type={'email'} name={'email'} placeholder={'이메일'}><IoMailOutline /></IconInput>
-            <IconInput type={'date'} name={'birthDate'} placeholder={'생년/월/일'}><IoCalendarClearOutline /></IconInput>
-            <IconInput type={'text'} name={'school'} placeholder={'학교'}><IoSchoolOutline /></IconInput>
-            <IconInput type={'text'} name={'grade'} placeholder={'학년'}><IoSchoolOutline /></IconInput>
-            <IconInput type={'text'} name={'class'} placeholder={'반'}><IoSchoolOutline /></IconInput>
+          gap: '12rem',
+          margin: '0 auto',
+        }}
+      >
+        <Logo />
+        <Form>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+              gap: '2rem',
+            }}
+          >
+            <IconInput type={'text'} name={'id'} placeholder={'아이디'}>
+              <IoPersonOutline />
+            </IconInput>
+            <IconInput
+              type={'password'}
+              name={'password'}
+              placeholder={'비밀번호'}
+            >
+              <IoLockClosedOutline />
+            </IconInput>
+            <IconInput type={'email'} name={'email'} placeholder={'이메일'}>
+              <IoMailOutline />
+            </IconInput>
+            <IconInput
+              type={'date'}
+              name={'birthDate'}
+              placeholder={'생년/월/일'}
+            >
+              <IoCalendarClearOutline />
+            </IconInput>
+            <IconInput type={'text'} name={'school'} placeholder={'학교'}>
+              <IoSchoolOutline />
+            </IconInput>
+            <IconInput type={'text'} name={'grade'} placeholder={'학년'}>
+              <IoSchoolOutline />
+            </IconInput>
+            <IconInput type={'text'} name={'class'} placeholder={'반'}>
+              <IoSchoolOutline />
+            </IconInput>
           </div>
           <Button theme={'dark'}>등록</Button>
-        </form>
+        </Form>
       </div>
     </div>
   );
--- frontend/src/components/ui/molecules/Form.tsx
+++ frontend/src/components/ui/molecules/Form.tsx
@@ -1,1 +1,23 @@
-export {};
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const StyleForm = styled.button`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  gap: 5rem;
+  border: 0;
+  background-color: transparent;
+`;
+
+const Button: React.FC<Props> = (props: Props) => {
+  const { children } = props;
+
+  return <StyleForm>{children}</StyleForm>;
+};
+
+export default Button;
