ea111a8492a21c423a14ff33975006da32cd8dc0
yjein <yjein4220@naver.com>
2022-09-11 19:04:39 +0900

    style: Hr width 속성 추가

--- frontend/src/components/ui/atoms/Hr.tsx
+++ frontend/src/components/ui/atoms/Hr.tsx
@@ -3,19 +3,25 @@ import styled from 'styled-components';
 
 interface Props {
   height?: number;
+  width?: number;
   bgColor?: string;
 }
 
-export const StyleHr = styled.hr<{ height?: number; bgColor?: string }>`
-  height: ${({ height }) => (height ? height + 'vh' : 'vh')};
+export const StyleHr = styled.hr<{
+  height?: number;
+  width?: number;
+  bgColor?: string;
+}>`
+  height: ${({ height }) => (height ? height + 'vh' : '0vh')};
+  width: ${({ width }) => (width ? width + 'vh' : '100%')};
   border: 0;
   background-color: ${({ bgColor }) => (bgColor ? bgColor : '#000000')};
 `;
 
 const Hr: React.FC<Props> = (props: Props) => {
-  const { height, bgColor } = props;
+  const { height, width, bgColor } = props;
 
-  return <StyleHr height={height} bgColor={bgColor} />;
+  return <StyleHr height={height} width={width} bgColor={bgColor} />;
 };
 
 export default Hr;
