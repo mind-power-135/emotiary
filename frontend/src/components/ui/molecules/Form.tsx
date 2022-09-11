fb4013d37997953f23cbf2dcfc9c8a9e3ce36b1e
yjein <yjein4220@naver.com>
2022-09-11 18:26:08 +0900

    feat: Form flexDirection 속성 추가

--- frontend/src/components/ui/molecules/Form.tsx
+++ frontend/src/components/ui/molecules/Form.tsx
@@ -3,19 +3,21 @@ import styled from 'styled-components';
 
 interface Props {
   children?: React.ReactNode;
+  flexDirection?: string;
 }
 
-const StyleForm = styled.form`
+const StyleForm = styled.form<{ flexDirection?: string }>`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${({ flexDirection }) =>
+    flexDirection ? flexDirection : 'column'};
   width: fit-content;
   gap: 5rem;
 `;
 
 const Button: React.FC<Props> = (props: Props) => {
-  const { children } = props;
+  const { children, flexDirection } = props;
 
-  return <StyleForm>{children}</StyleForm>;
+  return <StyleForm flexDirection={flexDirection}>{children}</StyleForm>;
 };
 
 export default Button;
