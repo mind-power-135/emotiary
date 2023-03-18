26a77c7e6033108c6737a46cded7f9a68b58cb6a
yuna706 <riyu6027@naver.com>
2023-03-18 20:28:58 +0900

    test: 감정 리스트 test

--- src/test/java/com/mindpower/emotiary/EmotiaryApplicationTests.java
+++ src/test/java/com/mindpower/emotiary/EmotiaryApplicationTests.java
@@ -1,13 +1,19 @@
 package com.mindpower.emotiary;
 
+import com.mindpower.emotiary.main.MainDao;
+import com.mindpower.emotiary.main.MainService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmotiaryApplicationTests {
 
+	@Autowired
+	MainService mainService;
 	@Test
-	void contextLoads() {
+	void MainEmotionsList() {
+		mainService.selectEmotionsList();
 	}
 
 }
